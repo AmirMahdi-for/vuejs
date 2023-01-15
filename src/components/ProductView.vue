aeb9b2c255d0c302e945800d3e02c0552f35dc5e
AmirMahdi-for <97610864+AmirMahdi-for@users.noreply.github.com>
2023-01-15 18:00:53 +0330

    learn v-for in vueJs

--- src/components/ProductView.vue
+++ src/components/ProductView.vue
@@ -5,6 +5,26 @@
         {{ my_data.name }}
         <br>
         <a :href="my_data.web_url">THIS IS GOOGLE</a>
+        <br>
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Job</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="person, i in people" :key="person.firstName">
+                    <td> {{ i+1 }} </td>
+                    <td> {{ person.firstName + ' ' + person.lastName }} </td>
+                    <td> {{ person.age }} </td>
+                    <td> {{ person.job }} </td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
     </div>
 </template>
 
@@ -18,7 +38,28 @@
                     name : 'amir mahdi',
                     age : '20',
                     web_url : 'http:///google.com',
-                }
+                },
+
+                people : [
+                    {
+                        firstName : 'Amir',
+                        lastName : 'Fakhri',
+                        age : '28',
+                        job : 'Co-founder'
+                    },
+                    {
+                        firstName : 'Kamand',
+                        lastName : 'Soleymani',
+                        age : '25',
+                        job : 'Manager'
+                    },
+                    {
+                        firstName : 'Nilo',
+                        lastName : 'Tahmasb',
+                        age : '27',
+                        job : 'Developer'
+                    },
+                ]
             }
         },
     }
