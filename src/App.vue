054bdbd9c0a00b215a6fd69f587873894f7e884f
AmirMahdi-for <97610864+AmirMahdi-for@users.noreply.github.com>
2023-01-15 17:17:14 +0330

    first use data and component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
-    
-
-  </div>
+    <div>
+        <product-view></product-view>
+        <hr>
+        <h2>THIS IS App.vue</h2>
+    </div>
 </template>
 
 <script>
 
+// eslint-disable-next-line no-unused-vars
+import ProductView from './components/ProductView.vue';
 
 export default {
-
+    components : {
+        ProductView,
+    },
 }
 </script>
 
