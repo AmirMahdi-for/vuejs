786497a911c7c966aaeb0989269b465c218ec171
AmirMahdi-for <97610864+AmirMahdi-for@users.noreply.github.com>
2023-01-15 17:34:46 +0330

    learn object in vue and use v-bind for href

--- src/components/ProductView.vue
+++ src/components/ProductView.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         {{ message }}
+        <br>
+        {{ my_data.name }}
+        <br>
+        <a :href="my_data.web_url">THIS IS GOOGLE</a>
     </div>
 </template>
 
@@ -8,9 +12,15 @@
     export default {
         data()  {
             return {
-                message : 'salam vuejs'
+                message : 'HelloVueJs',
+
+                my_data : {
+                    name : 'amir mahdi',
+                    age : '20',
+                    web_url : 'http:///google.com',
+                }
             }
-        }
+        },
     }
 </script>
 
