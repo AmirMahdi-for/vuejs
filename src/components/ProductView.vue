88a7aa001b45f53d0e5fd5ddf26e4e2586315664
AmirMahdi-for <97610864+AmirMahdi-for@users.noreply.github.com>
2023-01-16 13:42:36 +0330

    learn IF

--- src/components/ProductView.vue
+++ src/components/ProductView.vue
@@ -2,8 +2,10 @@
     <div>
         {{ message }}
         <br>
+        <!-- LEARN OBJECT -->
         {{ my_data.name }}
         <br>
+        <!-- LEARN V-FOR -->
         <a :href="my_data.web_url">THIS IS GOOGLE</a>
         <br>
         <table>
@@ -16,7 +18,7 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="person, i in people" :key="person.firstName">
+                <tr v-for="person, i in people" :key="person.id">
                     <td> {{ i+1 }} </td>
                     <td> {{ person.firstName + ' ' + person.lastName }} </td>
                     <td> {{ person.age }} </td>
@@ -25,6 +27,16 @@
             </tbody>
         </table>
         <br>
+        <!-- LEARN V-IF -->
+        <div v-if="list.length > 0">
+            <div v-for="person in list" :key="person.name">
+                <h2> {{ person.name }} </h2>
+                <p>craft : <span>{{person.craft}}</span></p>
+            </div>
+        </div>
+        <div v-else>
+            Loading
+        </div>
     </div>
 </template>
 
@@ -43,28 +55,32 @@
 
                 people : [
                     {
+                        id : 'a',
                         firstName : 'Amir',
                         lastName : 'Fakhri',
                         age : '28',
                         job : 'Co-founder'
                     },
                     {
+                        id : 'b',
                         firstName : 'Kamand',
                         lastName : 'Soleymani',
                         age : '25',
                         job : 'Manager'
                     },
                     {
+                        id : 'c',
                         firstName : 'Nilo',
                         lastName : 'Tahmasb',
                         age : '27',
                         job : 'Developer'
                     },
                 ],
+                list : [],
             }
         },
         mounted : function () {
-            axios.get('http://api.open-notify.org/astros.json').then(res => console.log(res));
+            axios.get('http://api.open-notify.org/astros.json').then(res => this.list = res.data.people);
         },
     }
 </script>
