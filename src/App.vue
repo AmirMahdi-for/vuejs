eea9f34f64ca9b1b92438ee6180b014e5e25f689
AmirMahdi-for <97610864+AmirMahdi-for@users.noreply.github.com>
2023-01-21 12:42:09 +0330

    first commit for SabzLearn and strat this course

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <ProductView></ProductView>
+        <!-- <ProductView></ProductView> -->
+        <!-- <FormList></FormList> -->
+        <SabzLearn></SabzLearn>
         <hr>
         <h2>THIS IS App.vue</h2>
 
@@ -11,10 +13,16 @@
 
 // eslint-disable-next-line no-unused-vars
 import ProductView from './components/ProductView.vue';
+// eslint-disable-next-line no-unused-vars
+import FormList from './components/FormList.vue';
+// eslint-disable-next-line no-unused-vars
+import SabzLearn from './components/SabzLearn.vue';
 
 export default {
     components : {
-        ProductView,
+        // ProductView,
+        // FormList,
+        SabzLearn,
     },
 }
 </script>
---
+++ src/components/SabzLearn.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>Counter : {{ counter }}</p>
+        <button @click="addCounter"> Add to Counter </button>
+        <button @click="minesCounter"> mines to Counter </button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                counter : 0,
+            }
+        },
+        methods : {
+            addCounter : function() {
+                this.counter++;
+            },
+            minesCounter : function() {
+                if(this.counter == 0){
+                    alert("can not mines to counter");
+                }
+                else{
+                    this.counter--;
+                }
+            }
+        }
+    }
+</script>
+    
+<style>
+
+</style>
