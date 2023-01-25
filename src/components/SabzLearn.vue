2a9e9313a8ebb5b1fa1a2e10ea32bd0f8e03c6e8
AmirMahdi-for <97610864+AmirMahdi-for@users.noreply.github.com>
2023-01-25 12:08:35 +0330

    learn watch

--- src/components/SabzLearn.vue
+++ src/components/SabzLearn.vue
@@ -1,51 +1,7 @@
 <template>
     <div>
-        <!-- <p>Counter : {{ counter }}</p>
-        <button @click="addCounter"> Add to Counter </button>
-        <button @click="minesCounter"> mines to Counter </button><br>
-        <a :href="link">ZZZZZZZZ</a>
-        <div v-html="title"></div>
-        <input type="text" v-model="value">
-        <b>{{ value }}</b>
-        <br><br>
-        <button @click="changeValue()">CHANGE</button>
-        <hr>
-        <button @click="number++"> Add to Number </button>
-        <p>{{ number }}</p>
-        <p v-if="number < 10">
-            number less than 10
-        </p>
-        <p v-else>
-            number more than 10
-        </p>
-        <h3 v-show="test">show this TAG</h3>
-        <ul v-for="an, i in animals" :key="i">
-            <li>{{ i+1 }}. {{ an }}</li>
-        </ul>
-        <ul v-for="person, i, id in people" :key="i">
-            <li>{{id+1}}.{{i}} : {{person}}</li>
-        </ul> -->
-        <input type="text" v-model="newToDo">
-        <button class="btn btn-success sm" @click="addTodo">add TODO</button>
-        <ul v-for="x, i in toDo" :key="i">
-            <li>
-                {{ x }}
-                <br>
-                <button class="btn btn-danger sm" @click="removeTodo(i)">remove TODO</button>
-            </li>
-        </ul>
-
-        <hr>
-
-        <button @click="num1++">num1</button>
-        <button @click="num2++">num2</button>
-
-        <h3>{{num1}}</h3>
-        <h3>{{num2}}</h3>
-
-        <h2>{{addNum1}}</h2>
-        <h2>{{addNum2}}</h2>
-
+        GB: <input type="text" v-model="GB">
+        MB: <input type="text" v-model="MB">
     </div>
 </template>
 
@@ -53,57 +9,16 @@
     export default {
         data() {
             return {
-                counter : 0,
-                link : "http://google.com",
-                title : "<h1>Google</h1>",
-                value : "this is new course",
-                number : 0,
-                test : true,
-                animals : ['lion', 'bird', 'elephant', 'tiger'],
-                people : {
-                    firstName : 'amirMahdi',
-                    lastName : 'amiri'
-                },
-                toDo : ['study', 'goOut', 'coding', 'smoking'],
-                newToDo : '',
-                num1 : 0,
-                num2 : 0,
-                ten : 10,
+                GB : null,
+                MB : null,
             }
         },
-        computed : {
-            addNum1: function (){
-                return this.num1 + this.ten
+        watch: {
+            GB: function (val) {
+                this.MB = val * 1024;
             },
-            addNum2: function (){
-                return this.num2 + this.ten
-            }
-        },
-        methods : {
-            addTodo: function(){
-                if (this.newToDo == '')  {
-                    alert('please add to todo')
-                }
-                else {
-                    this.toDo.push(this.newToDo);
-                }
-            },
-            removeTodo: function(i){
-                this.toDo.shift(this.toDo[i])
-            },
-            addCounter : function() {
-                this.counter++;
-            },
-            minesCounter : function() {
-                if(this.counter == 0){
-                    alert("can not mines to counter");
-                }
-                else{
-                    this.counter--;
-                }
-            },
-            changeValue: function(){
-                this.value = 'Change IT'
+            MB: function () {
+            
             }
         }
     }
