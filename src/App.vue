2b9615de52a6e475a783fa425cb2eac25c8e4552
AmirMahdi-for <97610864+AmirMahdi-for@users.noreply.github.com>
2023-02-12 14:51:01 +0330

    learn flex in youtube

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
     <div>
         <user></user>
         <hr>
+        <FlexBox></FlexBox>
+        <hr>
         <h2>THIS IS App.vue</h2>
 
     </div>
@@ -11,10 +13,12 @@
 
 // eslint-disable-next-line no-unused-vars
 import user from './components/UserPro.vue';
+import FlexBox from './components/FlexBox.vue'
 
 export default {
     components : {
         user,
+        FlexBox,
     },
 }
 </script>
---
+++ src/components/FlexBox.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <div class="search">search</div>
+        <div class="home">home</div>
+        <div class="login">login</div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    body{
+        font-size: 16px;
+    }
+
+    .container{
+        /* FLEX STYLE  */
+        display: flex;
+        flex-direction: row;
+
+
+        /* CSS STYLE  */
+        background-color: #8395a7;
+        border: 4px solid #222f3e;
+        height: 400px;
+    }
+
+    .container > div{
+        font-size: 1.4em;
+        color: white;
+        padding: 10px;
+        text-align: center;
+        
+    }
+
+    .search{
+        /* FLEX STYLE  */
+        flex: 1;
+        align-self: flex-start;
+        /* CSS STYLE  */
+        background-color: #01a3a4;
+    }
+
+    .home{
+        /* FLEX STYLE  */
+        flex: 3;
+        align-self: center;
+        /* CSS STYLE  */    
+        background-color: #ff9f43;
+    }
+
+    .login{
+        /* FLEX STYLE  */
+        flex: 1;
+        align-self: flex-end;
+        /* CSS STYLE  */
+        background-color: #f368e0;
+    }
+
+</style>
